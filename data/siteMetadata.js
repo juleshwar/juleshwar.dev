0990c9646fec7d8f00b34543353ba7409729f921
Joobie <dev.cleek@simplelogin.co>
2023-05-14 19:12:11 +0530

    docs: added personal LI, Github links

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -13,12 +13,11 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
+  email: 'juleshwar@pm.me',
+  github: 'https://github.com/juleshwar',
+  twitter: 'https://twitter.com/juleshwar',
   facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/juleshwar',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
