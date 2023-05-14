65ea1ebe570b5cf7073ade0d0a3b250ac29bb556
Joobie <dev.cleek@simplelogin.co>
2023-05-14 17:18:08 +0530

    build: minor edits to siteMetaData

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,14 +2,14 @@
 
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'juleshwar.dev',
+  author: 'Juleshwar',
+  headerTitle: 'juleshwar.dev',
+  description: 'TODO',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://juleshwar.dev',
+  siteRepo: 'https://github.com/juleshwar/juleshwar.dev',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
