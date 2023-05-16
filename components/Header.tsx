10085be0336d557172939b3ef00a13992ae4388c
Joobie <dev.cleek@simplelogin.co>
2023-05-17 00:14:31 +0530

    feat: modified logo to dynamically color it

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3">
-              <Logo />
+              <Logo className="fill-black dark:fill-white" />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
