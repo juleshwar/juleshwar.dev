8a0486389f95c3609d5b43a11e5d2e5933a800ef
Joobie <dev.cleek@simplelogin.co>
2023-05-22 02:41:26 +0530

    refactor: loading comments by default

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -31,7 +31,6 @@ interface LayoutProps {
 export default function PostLayout({ content, authorDetails, next, prev, children }: LayoutProps) {
   const { filePath, path, slug, date, title, tags } = content
   const basePath = path.split('/')[0]
-  const [loadComments, setLoadComments] = useState(false)
 
   return (
     <SectionContainer>
@@ -99,10 +98,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                   className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300"
                   id="comment"
                 >
-                  {!loadComments && (
-                    <button onClick={() => setLoadComments(true)}>Load Comments</button>
-                  )}
-                  {loadComments && <Comments commentsConfig={siteMetadata.comments} slug={slug} />}
+                  <Comments commentsConfig={siteMetadata.comments} slug={slug} />
                 </div>
               )}
             </div>
